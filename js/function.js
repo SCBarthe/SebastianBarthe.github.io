a9743036083dd86376421b66d985f4dc633a3438
Sebastián Barthe <89556771+SCBarthe@users.noreply.github.com>
2023-02-28 10:49:15 -0300

    Primera subida del portfolio
    Es la entrega de un proyecto para Ticmas.

---
+++ js/function.js
@@ -0,0 +1,26 @@
+
+function saludo(){
+    let nombre = document.getElementById("nombretxt").value; 
+    alert("Gracias por contactarte   " + nombre);
+  }
+
+
+ /**  document.getElementById('botoncontacto').addEventListener('clic', function(){ contacto('hola a todes')}); */
+
+
+ function contacto(){ 
+ alert("gracias por sumarte a mi Red"); 
+ }
+
+ function insta(){
+  alert("seguime en instagram @sebastian7mo");
+ }
+
+ function micontacto(){
+  alert("Al final de la pagina podes llenar el formulario");
+ }
+
+
+
+
+     
